d277b2ea7e70587c7d8fb3896090163435b0a4f5
Avazbek Tursunqulov <83706840+Avazbek99@users.noreply.github.com>
2026-01-16 00:56:08 +0500

    Update app.py
    majburiy papka yaratish

--- news/app.py
+++ news/app.py
@@ -4,6 +4,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
 
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
+
+os.makedirs(INSTANCE_DIR, exist_ok=True)
+
 # --------------------
 # APP CONFIG
 # --------------------
